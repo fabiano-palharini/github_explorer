1fdeb5604055b0b4a3c9115e8b14f4f8c8f9ddf9
Palharini <Fabiano_Palharini@Dell.com>
2020-04-26 13:59:04 -0300

    layout for repositories

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import logo from '../../assets/logo.svg';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import { Title, Form } from './styles';
 
 const Dashboard: React.FC = () => {
-    return <Title>Explore Github repositories</Title>
+    return (
+        <>
+            <img src={logo} alt="Github Explorer" />
+            <Title>Explore Github repositories</Title>
+
+            <Form>
+                <input placeholder="Type the repository name here"/>
+                <button type="submit">Search</button>
+            </Form>
+        </>
+    );
 }
 
 export default Dashboard;
